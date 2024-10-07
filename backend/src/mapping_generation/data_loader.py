29b20be21951a4744441843e0f1587b315b1f442
Komal Gilani <komalsyeda29@gmail.com>
2024-10-07 16:53:44 +0200

    updated mapping template

--- backend/src/mapping_generation/data_loader.py
+++ backend/src/mapping_generation/data_loader.py
@@ -118,7 +118,7 @@ def custom_data_loader(source_path):
             query_dict = {
                 "name": name,
                 "full_query": full_query,
-                'base_entity': f"{label},{visits}",
+                'base_entity': f"{label} {visits}",
                 "categories": categories,
                 "unit": unit,
                 "formula": formula,
