113061e2d67e6056cb67d76786ee9b753d3459cb
Komal Gilani <komalsyeda29@gmail.com>
2024-10-28 15:04:31 +0100

    mapping template bugs related to some re-ranking examples fixed

--- backend/src/mapping_generation/llm_chain.py
+++ backend/src/mapping_generation/llm_chain.py
@@ -631,14 +631,10 @@ def generate_ranking_prompt(query, documents, domain=None, in_context=True):
         print(f"{len(ranking_examples)}:ranking examples loaded")
         examples = get_relevant_examples(query, "ranking", ranking_examples, topk=1, min_score=0.6)
         # logger.info(f"selected_examples for Ranking Prediction={examples}")
-        human_template = """Objective: Rank candidate terms from the Standard Medical Terminologies/Vocabularies (SNOMED, LOINC, MeSH, ATC, UCUM, RxNorm, OMOP Extension) based on their relevance and closeness in meaning to a given medical query.
+        human_template = """Objective: Rank candidate terms from the Standard Medical Terminologies/Vocabularies (SNOMED, LOINC, MeSH, ATC, UCUM, RxNorm, OMOP Extension) according to the similarity between them and the entity in the query.
             **Instructions:For each candidate term, evaluate its relevance and closeness in meaning to the given query in a clinical context on a scale from 0 to 10, where:
                 *10: The candidate term is an accurate and an exact match/synonym to the input.
                 *0: The candidate term is completely irrelevant to the query.
-            **Scoring Guidance: Focus on the following aspects to determine the relevance of the candidate terms:
-                *Exact Match: Does the term precisely match or act as a synonym for the intended concept in the query? If yes, score closer to 10.
-                *Specificity: If the candidate terms are more specific than the query, determine which term adds relevant detail without deviating from the concept. Prioritize relevance to the core meaning of the query.
-                *General Relevance: If the candidate terms are broad or generic, identify which term still captures the main idea or essence of the query. Consider how well it fits in a clinical context, without being overly broad or irrelevant.
             **Examples: if provided Follow the examples to understand how to rank candidate terms based on their relevance to the query.
             **Desired format: Your response should be a list of dictionaries, each containing the keys "answer", "score", and "explanation". I repeat, provide the output in list of dictionaries format with the following fields: 'answer', 'score', and 'explanation'.
             Begin your response with the '[' and include no extra comments or information.
