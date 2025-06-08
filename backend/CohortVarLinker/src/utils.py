82af32af5a6a0947dcf2f5cc9b249605d83218cf
anas-elghafari <anas.elghafari@gmail.com>
2025-06-09 00:30:01 +0300

    edit is_categorical_variable: robust column normalization and allowing flexible names for variables column

--- backend/CohortVarLinker/src/utils.py
+++ backend/CohortVarLinker/src/utils.py
@@ -224,10 +224,16 @@ def extract_tick_values(texts: str) -> list[float]:
                 pass
     return ticks
 def is_categorical_variable(df):
+    #normalizing all possible column name variations by converting to uppercase
+    df.columns = [c.upper() for c in df.columns]
     binary_categorical = []
     multi_class_categorical = []
+    #allowing some flexibility in the name of the variable column
+    varname_col = [x for x in ['VARIABLE NAME', 'VARIABLENAME', 'VAR NAME'] 
+                    if x in df.columns][0]
+
     # create dict using variable name and CATREGORICAL
-    column_dict = dict(zip(df['variablename'], df['categorical']))
+    column_dict = dict(zip(df[varname_col], df['CATEGORICAL']))
     for key, value in column_dict.items():
         # if pd.notna(value) and value:
             if pd.notna(value) and value != "":
