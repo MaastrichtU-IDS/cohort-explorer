9a21f281ede0ac91d47ddbdd8d9d4f880cccf65e
anas-elghafari <anas.elghafari@gmail.com>
2025-08-13 20:54:52 +0200

    fixing how domain is checked

--- backend/src/upload.py
+++ backend/src/upload.py
@@ -267,9 +267,10 @@ def load_cohort_dict_file(dict_path: str, cohort_id: str) -> Dataset:
                     f"Row {i+2} (Variable: '{var_name_for_error}') has an invalid data type: '{row['VAR TYPE']}'. Accepted types: {', '.join(ACCEPTED_DATATYPES)}."
                 )
 
-            if row['DOMAIN'] not in {"condition_occurrence", "visit_occurrence", "procedure_occurrence", "measurement", "drug_exposure", "device_exposure", "person", "observation", "observation_period", "death", "specimen"}:
+            acc_domains = ["condition_occurrence", "visit_occurrence", "procedure_occurrence", "measurement", "drug_exposure", "device_exposure", "person", "observation", "observation_period", "death", "specimen"]
+            if row['DOMAIN'] not in acc_domains:
                 errors.append(
-                    f'Row {i+2} (Variable: "{var_name_for_error}") has an invalid domain: "{row["DOMAIN"]}". Accepted domains: {", ".join(["condition_occurrence", "visit_occurrence", "procedure_occurrence", "measurement", "drug_exposure", "device_exposure", "person", "observation", "observation_period", "death", "specimen"])}.'
+                    f'Row {i+2} (Variable: "{var_name_for_error}") has an invalid domain: "{row["DOMAIN"]}". Accepted domains: {", ".join(acc_domains)}.'
                 )
 
             # Handle Category validation (from 'categories' column created by parse_categorical_string)
