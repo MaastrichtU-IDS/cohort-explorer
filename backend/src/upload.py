71d8e9052abe6e68145d85d161610f043f537c7e
anas-elghafari <anas.elghafari@gmail.com>
2025-08-27 01:21:30 +0200

    minor fix for ucum:percent

--- backend/src/upload.py
+++ backend/src/upload.py
@@ -392,6 +392,8 @@ def load_cohort_dict_file(dict_path: str, cohort_id: str) -> Dataset:
                                 cat_code_uri = curie_converter.expand(code_to_check)
                                 if cat_code_uri: # Only add if valid and expanded
                                     #print(f"Adding category code {cat_code_uri} for category {category['value']} in cohort {cohort_id}, line {i}")
+                                    # Another temp fix just for TIM-HF!!
+                                    cat_code_uri = cat_code_uri.lower().replace("ucum:%", "ucum:percent")
                                     g.add((cat_uri, ICARE.conceptId, URIRef(cat_code_uri), cohort_uri))
         
         if len(warnings) > 0: # Log warnings even if processing succeeds
