33068830e3103099b25171d958d53dff27141e78
anas-elghafari <anas.elghafari@gmail.com>
2025-08-27 12:53:53 +0200

    turn off validation checking for concept codes

--- backend/src/upload.py
+++ backend/src/upload.py
@@ -394,11 +394,12 @@ def load_cohort_dict_file(dict_path: str, cohort_id: str) -> Dataset:
                         if index < len(categories_codes):
                             code_to_check = categories_codes[index].strip()
                             if code_to_check and code_to_check.lower() != "na":
+                                code_to_check = code_to_check.lower().replace("ucum:%", "ucum:percent").replace("[", "").replace("]", "")
                                 cat_code_uri = curie_converter.expand(code_to_check)
                                 if cat_code_uri: # Only add if valid and expanded
                                     #print(f"Adding category code {cat_code_uri} for category {category['value']} in cohort {cohort_id}, line {i}")
                                     # Another temp fix just for TIM-HF!!
-                                    cat_code_uri = cat_code_uri.lower().replace("ucum/%", "ucum/percent").replace("[", "").replace("]", "")
+                                    # cat_code_uri = cat_code_uri.lower().replace("ucum/%", "ucum/percent").replace("[", "").replace("]", "")
                                     #print(f"Adding category code {cat_code_uri} for category {category['value']} in cohort {cohort_id}, line {i}, cat_uri: {cat_uri}, conceptId: {ICARE.conceptId}")
                                     g.add((cat_uri, ICARE.conceptId, URIRef(cat_code_uri), cohort_uri))
         
