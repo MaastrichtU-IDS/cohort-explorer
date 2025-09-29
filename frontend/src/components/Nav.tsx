4b22307e82cb9bae30be414cadd8d412e391eae0
anas-elghafari <anas.elghafari@gmail.com>
2025-09-29 10:07:01 +0200

    Fix tabIndex type error in Nav.tsx - change string to number

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -129,10 +129,10 @@ export function Nav() {
           </li>
         </ul>
         <div className="dropdown lg:hidden">
-          <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
-          <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><Link href="/upload">Upload</Link></li>
             <li><Link href="/cohorts">Explore</Link></li>
             <li><Link href="/mapping">Mapping</Link></li>
