5f45aa08491ed8919a7c659ae4c02e771eee5be0
anas-elghafari <anas.elghafari@gmail.com>
2025-11-25 08:54:06 +0100

    Fix Next.js build error by simplifying button text

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -170,7 +170,7 @@ export function Nav() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
-                Go to the DCR on Decentriq's Platform
+                Go to the created DCR (On The Decentriq Platform)
               </a>
             </div>
             <p className="mb-2">Title: {result.dcr_title}</p>
