1ae205e29e0a4b12c6b937f30287d81f8268d74d
Komal Gilani <komalsyeda29@gmail.com>
2026-08-13 00:29:19 +0200

    Update config.py

--- backend/cross_mapping/src/config.py
+++ backend/cross_mapping/src/config.py
@@ -43,7 +43,7 @@ class Settings:
         "code": "loinc:39156-5",
         "label": "Body mass index (BMI) [Ratio]",
         "unit": "ucum:kg/m2",
-        "required_omops": [3036277, 3025315],  #  Height,Weight
+        "required_omops": [3036277, 3025315],  #  Body Height, Body Weight
         "category": "measurement",
         "data_type": "continuous_variable"
     },
@@ -177,7 +177,7 @@ class Settings:
         4083587,  # Date of birth
         4231970,  # Date of visit
     ],
-    "category": "person",
+    "category": "measurement",
     "data_type": "continuous_variable",
     "formula": "completed calendar years between date of birth and baseline date",
 }
